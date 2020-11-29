7e01e69b81e9f1c1d458ea7944e570e0dccd9ec7
Konstantin Köhring <konstantin.koehring@tu-dresden.de>
2020-11-29 22:22:57 +0100

    Base Commit for Qontakt-Host

---
+++ qontakt-host/src/main/java/app/qontakt/host/QontaktHostApplication.java
@@ -0,0 +1,13 @@
+package app.qontakt.host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QontaktHostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QontaktHostApplication.class, args);
+    }
+
+}
---
+++ qontakt-host/src/test/java/app/qontakt/host/QontaktHostApplicationTests.java
@@ -0,0 +1,13 @@
+package app.qontakt.host;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QontaktHostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
