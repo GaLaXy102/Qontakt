2355e447d803cc9745d09887dc3f60ddd7d1c025
Konstantin Köhring <konstantin.koehring@tu-dresden.de>
2020-11-29 22:19:15 +0100

    Base Commit for Qontakt-User

---
+++ qontakt-user/src/main/java/app/qontakt/user/QontaktUserApplication.java
@@ -0,0 +1,13 @@
+package app.qontakt.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QontaktUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QontaktUserApplication.class, args);
+    }
+
+}
