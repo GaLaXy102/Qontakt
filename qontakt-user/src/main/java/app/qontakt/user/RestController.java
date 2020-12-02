451082543061e7f36c0adad54bbf72851509b823
ylvion <ylvi@ylja.de>
2020-12-02 14:33:38 +0100

    Implementation of /visit endpoint

--- qontakt-user/src/main/java/app/qontakt/user/RestController.java
+++ qontakt-user/src/main/java/app/qontakt/user/RestController.java
@@ -3,6 +3,7 @@ package app.qontakt.user;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,11 +11,22 @@ import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+/**
+ * REST Controller for User administration
+ */
 @org.springframework.web.bind.annotation.RestController
+@RequestMapping("/api/v1/user")
 public class RestController {
 
     private final UserService userService;
 
+    /**
+     * Check if User with the given UID is logged in
+     *
+     * @param request
+     * @param user_uid UID of the User
+     * @return true if User is logged in
+     */
     public static boolean isAuthorized(HttpServletRequest request, String user_uid) {
         return user_uid.equals(request.getHeader("X-User"));
     }
@@ -23,7 +35,15 @@ public class RestController {
         this.userService = userService;
     }
 
-    @PutMapping("/api/v1/user/visit")
+    /**
+     * Add a new Visit for the given User and Lokal
+     *
+     * @param user_uid  UID of the User
+     * @param local_uid UID of the Lokal
+     * @param request
+     * @return true if creation of Visit is successful and User has no unterminated Visits
+     */
+    @PutMapping("/visit")
     ResponseEntity<Boolean> newVisit(@RequestParam Optional<String> user_uid, @RequestParam String local_uid, HttpServletRequest request) {
         if (user_uid.isEmpty()) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
