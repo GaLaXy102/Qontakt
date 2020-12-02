50bf3ba6021653a01f10be88c3dc3db99248ecbc
Konstantin Köhring <konstantin.koehring@tu-dresden.de>
2020-12-02 16:23:12 +0100

    Update OpenAPI and Swagger Configuration

--- qontakt-host/src/main/java/app/qontakt/host/QontaktHostApplication.java
+++ qontakt-host/src/main/java/app/qontakt/host/QontaktHostApplication.java
@@ -1,5 +1,12 @@
 package app.qontakt.host;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +16,37 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class QontaktHostApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(QontaktHostApplication.class, args);
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("user-header",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-User")
+                        )
+                )
+                .addServersItem(new Server().url("https://localhost"))
+                .addServersItem(new Server().url("https://qontakt.me"))
+                .info(new Info()
+                        .title("Qontakt Host Service")
+                        .contact(new Contact()
+                                .name("Galaxy102")
+                                .email("services@galaxion.de")
+                                .url("https://www.galaxy102.de"))
+                        .license(new License().name("MIT Licence")))
+                ;
+    }
+
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(QontaktHostApplication.class, args);
     }
 
 }
