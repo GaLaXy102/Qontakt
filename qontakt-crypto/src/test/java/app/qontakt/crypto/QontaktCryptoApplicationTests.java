88985d4b1d7e6d838b89cee5be85d9c546b9baf6
Konstantin Köhring <konstantin.koehring@tu-dresden.de>
2020-12-31 14:58:40 +0100

    Add crypto module

---
+++ qontakt-crypto/src/test/java/app/qontakt/crypto/QontaktCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.qontakt.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QontaktCryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
